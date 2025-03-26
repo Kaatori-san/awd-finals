d10e77afded64e77e717a97b54f72d1ccfaf68d6
Kaatori <kurtzedrikdy+github@gmail.com>
2025-03-26 09:20:46 +0800

    Delete assets directory

--- assets/js/cart.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleSummary = document.querySelector(".toggle-summary");
-    const orderSummary = document.querySelector(".order-summary");
-
-    toggleSummary.addEventListener("click", function () {
-        orderSummary.classList.toggle("active");
-    });
-});
--- assets/js/navbar.js
+++
@@ -1,3 +0,0 @@
-$(function () {
-  $("#navbar").load("./assets/navbar.html");
-});
